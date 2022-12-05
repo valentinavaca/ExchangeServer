51ea1da9c88584c354e939a8f4c9a6d8b84712e8
valentinavaca <67397877+valentinavaca@users.noreply.github.com>
2022-12-04 22:02:57 -0800

    trial 16

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -42,54 +42,7 @@ def check_sig(payload, sig):
         return algosdk.util.verify_bytes(json.dumps(payload).encode('utf-8'), sig, pk)
 
 def fill_order(order, txes=[]):
-    for unfilled_order in txes:
-        if find_match(order, unfilled_order):
-            order.filled = datetime.now()
-            unfilled_order.filled = datetime.now()
-            order.counterparty_id = unfilled_order.id
-            unfilled_order.counterparty_id = order.id
-            g.session.commit()
-            if order.buy_amount > unfilled_order.sell_amount:
-                sender_pk = order.sender_pk
-                receiver_pk = order.receiver_pk
-                buy_currency = order.buy_currency
-                sell_currency = order.sell_currency
-                buy_amount = order.buy_amount - unfilled_order.sell_amount
-                sell_amount = 1.1*(buy_amount * order.sell_amount/order.buy_amount )
-                creator_id = order.id
-                new_order = Order(
-                    sender_pk = sender_pk, 
-                    receiver_pk = receiver_pk, 
-                    buy_currency = buy_currency,
-                    sell_currency = sell_currency,
-                    buy_amount = buy_amount,
-                    sell_amount = sell_amount,
-                    creator_id = creator_id,
-                )
-                unfilled_orders = g.session.query(Order).filter(Order.filled==None).all()
-                fill_order(new_order, unfilled_orders)
-                break
-            if unfilled_order.sell_amount > order.buy_amount:
-                sender_pk = unfilled_order.sender_pk
-                receiver_pk = unfilled_order.receiver_pk
-                buy_currency = unfilled_order.buy_currency
-                sell_currency = unfilled_order.sell_currency
-                sell_amount = unfilled_order.sell_amount - order.buy_amount
-                buy_amount = 0.9 * ( sell_amount * unfilled_order.buy_amount / unfilled_order.sell_amount )
-                creator_id = unfilled_order.id
-                new_order = Order(
-                    sender_pk = sender_pk, 
-                    receiver_pk = receiver_pk, 
-                    buy_currency = buy_currency,
-                    sell_currency = sell_currency,
-                    buy_amount = buy_amount,
-                    sell_amount = sell_amount,
-                    creator_id = creator_id,
-                )
-                unfilled_orders = g.session.query(Order).filter(Order.filled==None).all()
-                fill_order(new_order, unfilled_orders)
-                break
-            break
+    pass
   
 def log_message(d):
     # Takes input dictionary d and writes it to the Log table
